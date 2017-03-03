04e7a7854922e5ba599f6c27c3521737989eedca
MSI_MANTAS2\MantasMSI <mantasling@gmail.com>
2017-03-03 13:43:11 +0100

    first comit

---
+++ src/com/company/Person.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ * Created by MantasMSI on 3/3/2017.
+ */
+public class Person {
+
+    private String name;
+    private int age;
+
+    public int getAge() {
+
+        return age;
+    }
+
+    public int setAge(int a){
+
+        return a;
+    }
+
+
+
+}
